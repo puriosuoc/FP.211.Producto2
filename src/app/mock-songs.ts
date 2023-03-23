13e43e6867c94a8027ebd555a22a5a60b14d1b13
puriosuoc <purios@uoc.edu>
2023-03-23 19:17:03 +0100

    Commit inicial

--- src/app/mock-songs.ts
+++ src/app/mock-songs.ts
@@ -7,6 +7,6 @@ export const SONGS: Song[] = [
   { tittle: 'Sunset Sun Sky', subtittle: 'El Diluvi, Gertrudis', artist:'Rosalia', year:2022, disc:'El Gran Ball', style:'Clásica', url:'../../assets/cover4.jpg', discografica: 'random records', formato: 'digital'  },
   { tittle: 'Retrowave', subtittle: 'Judit Neddermann', artist:'Shakira', year:2020, disc:'Retrowave', style:'Power Metal', url:'../../assets/cover5.jpg', discografica: 'random records', formato: 'digital'  },
   { tittle: 'Una Lluna a L´Aigua', subtittle: 'ZOO', artist:'Macaco', year:2018, disc:'Lorem Ipsum', style:'Trap', url:'../../assets/cover6.jpg', discografica: 'random records', formato: 'digital'  },
-  { tittle: 'Bendita Vida', subtittle: 'Amparanoia, Muerdo', artist:'Two Doors Cinema Club', year:2011, disc:'El Gran Ball', style:'Rock', url:'../../assets/cover7.jpg', discografica: 'random records', formato: 'digital'  },
+  { tittle: 'Benditas Vida', subtittle: 'Amparanoia, Muerdo', artist:'Two Doors Cinema Club', year:2011, disc:'El Gran Ball', style:'Rock', url:'../../assets/cover7.jpg', discografica: 'random records', formato: 'digital'  },
   { tittle: 'Saying Goodbye in the Rain', subtittle: 'Koers', artist:'SFDK', year:1998, disc:'SUNSET SUN SKY', style:'Pop Rock', url:'../../assets/cover8.jpg', discografica: 'random records', formato: 'digital'  },
 ];

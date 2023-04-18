1e01fa07fcdb62fdaa84421b288655b260cfaf96
puriosuoc <purios@uoc.edu>
2023-04-18 19:17:22 +0200

    v2.1

--- src/app/mock-songs.ts
+++ src/app/mock-songs.ts
@@ -8,5 +8,5 @@ export const SONGS: Song[] = [
   { tittle: 'Retrowave', subtittle: 'Judit Neddermann', artist:'Shakira', year:2020, disc:'Retrowave', style:'Power Metal', image:'../../assets/cover5.jpg', url:'../../assets/song2.mp3', discografica: 'random records', formato: 'digital'  },
   { tittle: 'Una Lluna a L´Aigua', subtittle: 'ZOO', artist:'Macaco', year:2018, disc:'Lorem Ipsum', style:'Trap', image:'../../assets/cover6.jpg', url:'../../assets/song3.mp3', discografica: 'random records', formato: 'digital'  },
   { tittle: 'Benditas Vida', subtittle: 'Amparanoia, Muerdo', artist:'Two Doors Cinema Club', year:2011, disc:'El Gran Ball', style:'Rock', image:'../../assets/cover7.jpg', url:'../../assets/song1.mp3', discografica: 'random records', formato: 'digital'  },
-  { tittle: 'Saying Goodbye in the Rain', subtittle: 'Koers', artist:'SFDK', year:1998, disc:'SUNSET SUN SKY', style:'Pop Rock', image:'../../assets/cover8.jpg', url:'../../assets/song2.mp3', discografica: 'random records', formato: 'digital'  },
+  { tittle: 'Saying Goodbye in the Rain', subtittle: 'Koers', artist:'SFDK', year:1998, disc:'SUNSET SUN SKY', style:'Trap', image:'../../assets/cover8.jpg', url:'../../assets/song2.mp3', discografica: 'random records', formato: 'digital'  },
 ];
